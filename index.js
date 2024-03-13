1a3d62ae3dc3c45e4a458173792d86443ab42c17
koshyar <ensw03420@student.su.edu.krd>
2024-03-13 15:02:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let array = [];
+  for (let i = 0; i < names.length; i++) {
+    array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return array;
+}
+
+function countDown(num) {
+  for (let i = num; i >= 0; i--) console.log(i);
+}
+
+countDown(10);
